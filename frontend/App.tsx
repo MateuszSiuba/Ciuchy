1a98592188fe95a2b40c98ac506b35e30a347cb7
Mateusz Siuba <mateusz.siuba@tpvaoc.com>
2026-06-05 22:04:39 +0200

    Fix app status bar contrast

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 import { BottomTabNavigator } from './src/navigation/BottomTabNavigator';
 
 export default function App(): React.ReactElement {
   return (
     <SafeAreaProvider>
+      <StatusBar style="dark" />
       <BottomTabNavigator />
     </SafeAreaProvider>
   );
